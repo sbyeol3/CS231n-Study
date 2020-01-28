e139dcf94b068320047d332d45903cfbe3650732
saetbyeol <ahnsbyeol3@gmail.com>
2020-01-28 20:02:47 +0900

    linear_svm

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -28,18 +28,20 @@ def svm_loss_naive(W, X, y, reg):
     num_train = X.shape[0]
     loss = 0.0
     for i in range(num_train):
-        scores = X[i].dot(W)
+        scores = X[i].dot(W) # X랑 가중치 곱함
         correct_class_score = scores[y[i]]
         for j in range(num_classes):
             if j == y[i]:
                 continue
-            margin = scores[j] - correct_class_score + 1 # note delta = 1
+            margin = scores[j] - correct_class_score + 1 # 다른 클래스일 때
             if margin > 0:
                 loss += margin
+                dW[:,j] += X[i,:] # margin이 0보다 크게 하는 계수 Xnd를 더해줌
+                dW[:,y[i]] -= X[i,:] # 정답 클래스 스코어를 같이 빼줌
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
-    loss /= num_train
+    loss /= num_train # average 구하기
 
     # Add regularization to the loss.
     loss += reg * np.sum(W * W)
@@ -54,7 +56,9 @@ def svm_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    dW += reg * W
+    # loss를 w로 미분한 것 = dW , w가 변할 때 loss에 영향을 얼마나 주는가?
+    dW /= num_train # avg
+    dW += reg * W # regularization
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     
@@ -86,7 +90,7 @@ def svm_loss_vectorized(W, X, y, reg):
     margin[ np.arange(num_train), y] = 0 # ignore
     
     loss = margin.sum() / num_train
-    loss += reg * np.sum(W * W) # regularization
+    loss += 0.5 * reg * np.sum(W * W) # regularization
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -101,12 +105,11 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    margin[margin > 0] = 1
-    valid_margin_count = margin.sum(axis=1)
-    margin[np.arange(num_train),y ] -= valid_margin_count
+    dscores = np.zeros_like(scores)
+    dscores[margin > 0] = 1
+    dscores[np.arrage(num_train),y] -= np.sum(dscores, axis=1)
     dW = (X.T).dot(margin) / num_train
-
-    dW = dW + reg * 2 * W # Regularization
+    dW += reg * W # Regularization
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
